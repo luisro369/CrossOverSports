eef8c6e649c145372c77cf559802bcec4281da4f
luisro369 <00005513@uca.edu.sv>
2018-06-21 23:06:21 -0600

    mejorando codigo de tablayout

--- app/src/main/java/com/luisro00005513/crossoversports/Fragments/FragmentoHome.java
+++ app/src/main/java/com/luisro00005513/crossoversports/Fragments/FragmentoHome.java
@@ -50,11 +50,25 @@ public class FragmentoHome extends Fragment {
         mPager = (ViewPager) view.findViewById(R.id.viewPagerHome);
         mPagerAdapter = new ScreenSlidePagerAdapter(getChildFragmentManager());
         mPager.setAdapter(mPagerAdapter);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                mPager.setCurrentItem(tab.getPosition());
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
         return view;
     }
 
 
 
+    //clase del pager adapter
     private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
         public ScreenSlidePagerAdapter(FragmentManager fm) {
             super(fm);
