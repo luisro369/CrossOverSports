02c54117fb6f54d998095d9ce417f476e0db3e95
cspsama <sama.cesar7@gmail.com>
2018-07-05 22:10:09 -0600

    se añadieron las entidades principales para el room (pojo)

--- app/src/main/java/com/luisro00005513/crossoversports/Entities/Player.java
+++ app/src/main/java/com/luisro00005513/crossoversports/Entities/Player.java
@@ -1,8 +1,66 @@
 package com.luisro00005513.crossoversports.Entities;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity
 public class Player {
+
+    @PrimaryKey
+    @ColumnInfo(name="player_id")
+    private Integer playerId;
+
+    @ColumnInfo(name="player_name")
     private String playerName;
+
+    @ColumnInfo(name="player_alias")
     private String playerAlias;
+
+    @ColumnInfo(name="player_birth")
     private String playerBirth;
-    private String playerExtraInfo;
+
+    @ColumnInfo(name="player_extra")
+    private String playerExtra;
+
+
+    public Integer getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(Integer playerId) {
+        this.playerId = playerId;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public String getPlayerAlias() {
+        return playerAlias;
+    }
+
+    public void setPlayerAlias(String playerAlias) {
+        this.playerAlias = playerAlias;
+    }
+
+    public String getPlayerBirth() {
+        return playerBirth;
+    }
+
+    public void setPlayerBirth(String playerBirth) {
+        this.playerBirth = playerBirth;
+    }
+
+    public String getPlayerExtra() {
+        return playerExtra;
+    }
+
+    public void setPlayerExtra(String playerExtra) {
+        this.playerExtra = playerExtra;
+    }
 }
