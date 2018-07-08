113670ac54488583db4422c4aee4939ccc6eecaa
luisro369 <00005513@uca.edu.sv>
2018-07-08 17:17:25 -0600

    ya funciona tab explore tournament

--- app/src/main/java/com/luisro00005513/crossoversports/Fragments/FragmentExplore/FragmentExploreTournaments.java
+++ app/src/main/java/com/luisro00005513/crossoversports/Fragments/FragmentExplore/FragmentExploreTournaments.java
@@ -2,17 +2,32 @@ package com.luisro00005513.crossoversports.Fragments.FragmentExplore;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.luisro00005513.crossoversports.Fragments.FragmentExplore.adapter.PlayersAdapter;
+import com.luisro00005513.crossoversports.Fragments.FragmentExplore.adapter.TournamentsAdapter;
 import com.luisro00005513.crossoversports.R;
+import com.luisro00005513.crossoversports.retrofit.RetrofitServices;
+import com.luisro00005513.crossoversports.retrofit.extras.Player;
+import com.luisro00005513.crossoversports.retrofit.extras.Tournament;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FragmentExploreTournaments extends Fragment {
+
+    SwipeRefreshLayout swipeRefreshLayout;
+    RecyclerView recyclerView;
+    public static List<Tournament> listTournaments;
 
 
     public FragmentExploreTournaments() {
@@ -24,7 +39,75 @@ public class FragmentExploreTournaments extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explore_tournaments, container, false);
+        View view = inflater.inflate(R.layout.fragment_explore_tournaments, container, false);
+        //variables de views
+        swipeRefreshLayout = (SwipeRefreshLayout)view.findViewById(R.id.swipe_tournament_explore);
+
+
+        InitialView();
+
+        return view;
+    }//oncreateview
+
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(listTournaments != null) {
+            CreandoCardViews();
+        }
     }
+
+    //============seteando xml========
+    protected void InitialView(){
+        //==========llamada a objeto retrofit=========
+        Thread thread = new Thread(){
+            public void run(){
+                RetrofitServices player = new RetrofitServices();
+                listTournaments = player.getTournaments();
+            }//run in backgorund
+        };thread.start();
+
+        if(listTournaments == null) {
+            swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                @Override
+                public void onRefresh() {
+                    //----------------llamada a views y llenarlos con lista(tienen que estar en un if)----
+                    //-----aca se setean todas las views a lo que uno quiera
+                    if (listTournaments != null) {
+                        CreandoCardViews();
+                    }
+                    //------------------------------------------------
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            swipeRefreshLayout.setRefreshing(false);
+                        }
+                    }, 400);
+                }
+            });
+        }//si la lista esta null refresca
+
+        else{
+        }
+
+    }//initialview
+
+
+    private void CreandoCardViews(){
+        //=========codigo para CardView de players=============
+        recyclerView = (RecyclerView)getView().findViewById(R.id.recycler_tournament_players);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        TournamentsAdapter tournamentsAdapter = new TournamentsAdapter(getContext(),listTournaments);//modifique esto
+        recyclerView.setAdapter(tournamentsAdapter);
+        //=========codigo para CardView de players(fin)========
+    }
+
+
+
+
+
+
 
 }
---
+++ app/src/main/java/com/luisro00005513/crossoversports/Fragments/FragmentExplore/adapter/TournamentsAdapterViewHolder.java
@@ -0,0 +1,39 @@
+package com.luisro00005513.crossoversports.Fragments.FragmentExplore.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.luisro00005513.crossoversports.R;
+import com.luisro00005513.crossoversports.retrofit.extras.Player;
+import com.luisro00005513.crossoversports.retrofit.extras.Tournament;
+
+import java.util.List;
+
+/**
+ * Created by luisro on 7/8/18.
+ */
+
+public class TournamentsAdapterViewHolder extends RecyclerView.ViewHolder {
+    ImageView tournamentAvatar;
+    TextView tournamentName;
+    TextView tournamentCountry;
+    Context context;
+    List<Tournament> tournamentsList;
+
+    public TournamentsAdapterViewHolder(View itemView, List<Tournament> tournamentsList, Context context) {
+        super(itemView);
+        this.tournamentsList =  tournamentsList;
+        this.context = context;
+
+        tournamentAvatar = (ImageView)itemView.findViewById(R.id.tournament_avatar);
+        tournamentName = (TextView)itemView.findViewById(R.id.tournament_name);
+        tournamentCountry = (TextView)itemView.findViewById(R.id.tournament_country);
+
+
+
+    }//constructor
+
+}
