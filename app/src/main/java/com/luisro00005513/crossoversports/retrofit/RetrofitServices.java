9e514ba4935da0f2ae261cd56b872d4dbd511dec
luisro369 <00005513@uca.edu.sv>
2018-07-07 19:00:22 -0600

    falta optimizar retrofit

--- app/src/main/java/com/luisro00005513/crossoversports/retrofit/RetrofitServices.java
+++ app/src/main/java/com/luisro00005513/crossoversports/retrofit/RetrofitServices.java
@@ -18,8 +18,6 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-import static com.luisro00005513.crossoversports.Activities.MainActivity.service;
-import static com.luisro00005513.crossoversports.Activities.MainActivity.services;
 
 /**
  * Created by luisro on 7/6/18.
